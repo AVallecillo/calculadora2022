2a78aff542877c3b462b0bd8219758fbd485fb8d
Antonio Vallecillo <antoniovallecillomoreno@gmail.com>
2022-03-03 10:55:57 +0100

    cambio de nombre en el test

--- src/TestCalculadora.java
+++ src/TestCalculadora.java
@@ -7,13 +7,13 @@ class TestCalculadora {
     private final Calculadora c = new Calculadora();
 
     @Test
-    void addition() {
+    void suma() {
         assertEquals(2, c.suma(1, 1));
 		double s;
 		for (double a=-10.0;a<10.0;a+=1.0) {
 			for (double b=-10.0;b<10.0;b+=1.0) { 
 				s = c.suma(a,b);
-				assert s==a+b:"Error suma: a="+a+" b="+b+" s="+s;
+				assert s==a+b:"Error suma: a="+a+" b="+b+" s="+s;			
 			}
 		}
     }
