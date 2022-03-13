fb13d706894b16daf25c0911e80fcc2a01211f1c
Florin <Florin@DRAGON-ROJO>
2022-03-13 11:30:39 +0100

    Modificacion de mi test de calculadora

--- src/TestCalculadora_FlorinUMA.java
+++ src/TestCalculadora_FlorinUMA.java
@@ -10,23 +10,26 @@ import org.junit.jupiter.api.Test;
  *
  */
 class TestCalculadora_FlorinUMA {
-	Calculadora miCalculadora;
+	Calculadora_FlorinUMA miCalculadora;
 
 	@BeforeEach
 	void init() {
-		miCalculadora = new Calculadora();
+		miCalculadora = new Calculadora_FlorinUMA();
 	}
 
 	@Test
 	public void testSuma() {
 		Assertions.assertEquals(4.0, miCalculadora.suma(2, 2));
 		Assertions.assertNotEquals(0, miCalculadora.suma(1, 0));
+		Assertions.assertEquals(-200, miCalculadora.suma(-100, -100));
 	}
 
 	@Test
 	public void testMult() {
 		Assertions.assertEquals(4.0, miCalculadora.mult(2, 2));
 		Assertions.assertNotEquals(80.0, miCalculadora.mult(5, 5));
+		Assertions.assertEquals(0, miCalculadora.mult(0, 80));
+		Assertions.assertEquals(1.0, miCalculadora.mult(80, 0.0125));
 	}
 
 	@Test
@@ -41,11 +44,14 @@ class TestCalculadora_FlorinUMA {
 		Assertions.assertEquals(1, miCalculadora.fact(0));
 		Assertions.assertEquals(1, miCalculadora.fact(1));
 		Assertions.assertEquals(2, miCalculadora.fact(2));
+		Assertions.assertEquals(479001600, miCalculadora.fact(12)); //ultimo resultado del factorial dentro dentro del rango de int
 	}
 	
 	@Test
 	public void testEsPrimo() {
-		Assertions.assertTrue(miCalculadora.esPrimo(1));
+		Assertions.assertFalse(miCalculadora.esPrimo(-5));
+		Assertions.assertFalse(miCalculadora.esPrimo(0));
+		Assertions.assertFalse(miCalculadora.esPrimo(1));
 		Assertions.assertTrue(miCalculadora.esPrimo(2));
 		Assertions.assertTrue(miCalculadora.esPrimo(3));
 		Assertions.assertFalse(miCalculadora.esPrimo(4));
