46a501081705a4e30517ee357164b124593fa50f
alepa <alepa@J4RV1S>
2022-03-14 09:01:01 +0100

    alexpascual_14/03/2022_2

--- src/Calculadora_alexpascualm.java
+++ src/Calculadora_alexpascualm.java
@@ -79,7 +79,7 @@ public class Calculadora_alexpascualm implements ICalculadora {
 	 * @return  :boolean -- Resultado 
 	 */
 	public boolean esPrimo(int n) {
-		if(n<=0) return false;
+		if(n<=1) return false;
 		  for(int i=2;i<n;i++) {
 		        if(n%i==0)
 		            return false;
