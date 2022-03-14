a0b4f6928958751bed317e6c38965814e72fec8c
carlosbelop <belopcarlos@uma.es>
2022-03-14 09:03:23 +0100

    Calculadora Carlos

--- src/Calculadora.java
+++ src/Calculadora.java
@@ -1,93 +1,98 @@
 
 public class Calculadora implements ICalculadora {
-	
-	public double suma(double a,double b){
-		double sum = a+b;
-		if(sum >= Double.MAX_VALUE) {
-			
-			throw new ArithmeticException();
-		}
-		return sum;
-	}
-	
-	public double resta(double a,double b){
-		double rest = a-b;
-		if(rest == Double.MIN_VALUE) {
-			
-			throw new ArithmeticException();
-		}
-		return rest;
-	}
-	
-	public double mult(double a,double b){
-		double mult = a*b;
-		if(mult >= Double.MAX_VALUE) {
-			
-			throw new ArithmeticException();
-		}
-		return mult;
-	}
-	
-	public double divide(double a,double b){
-		double div = 0;
+/**
+ * El método suma dos valores
+ * @param a :double -- un sumando
+ * @param b :double -- otro sumando
+ * @return :double -- el resultado de la suma
+ * 
+ */
+	public double suma(double a, double b) {
 		
-		if(a == 0 && b != 0){
-			return div;
-		}else if(b == 0){
-			
-			throw new ArithmeticException();
-		}
-			div = a/b;
+		return a + b;
+	}
+	
+	/**
+	 * El método resta dos valores
+	 * @param a :double -- un valor
+	 * @param b :double -- el valor que se le resta
+	 * @return :double -- el resultado de la suma
+	 * 
+	 */
 
-		return div;
-		
+	public double resta(double a, double b) {
+		return Math.round((a-b)*1000d)/1000d;
 	}
-	public int fact(int n){
-		int factorial = 1;
-		
-		if(n == 0){
-			return 1;
-		}else{
-			for (int i=2; i<=n; i++){
-				factorial = factorial * i;
+	
+	/**
+	 * El método multiplica dos valores
+	 * @param a :double -- un factor
+	 * @param b :double -- otro factor
+	 * @return :double -- el resultado de la suma
+	 * 
+	 */
+
+	public double mult(double a, double b) {
+		return a * b;
+	}
+
+	/**
+	 * El metodo divide calcula la division de dos numeros reales
+	 * 
+	 * @param a :double -- el dividendo
+	 * @param b :double -- el divisor
+	 * @return :double -- el resultado de la division
+	 * @throws ArithmeticException if b==0) //precondition
+	 * @post result*b == a
+	 */
+	public double divide(double a, double b) {
+		double result;
+		if (b == 0)
+			throw new ArithmeticException();
+		result = a / b;
+		assert result * b == a;
+		return result;
+	}
+
+	/**
+	 * Calcula el factorial
+	 * @param a :int -- el valor al cual calcular el factorial
+	 * @return  :int -- el resultado del factorial
+	 * 
+	 */
+	public int fact(int n) {
+		int result = 1;
+		for (int x = 1; x < Math.abs(n); x++) {
+			result = result * x;
+		}
+		if (n != 0) {
+			result = result * n;
+		}
+		return result;
+	}
+	
+	/**
+	 * Ddevuelve si es primo o no
+	 * @param a :int -- el valor a comprobar
+	 * @return  :boolean -- el resultado
+	 * 
+	 */
+	
+	public boolean esPrimo(int n) {
+		if (n < 0) {
+			throw new ArithmeticException();
+		}
+		int cont = 0;
+		boolean prim = false;
+		for (int x = 1; x <= n; x++) {
+			if (n % x == 0) {
+				cont++;
 			}
 		}
-		if(factorial >= Integer.MAX_VALUE) {
-			
-			throw new ArithmeticException();
-
+		if (cont == 2) {
+			prim = true;
 		}
-		return factorial;
-	}
-	public boolean esPrimo(int n){
-		boolean primo = true;
-		for(int i = 2; i < n; i++) {
-			if (n % i == 0) {
-            primo = false;
-            break;
-			}
-		}
-		return primo;
+		return prim;
 	}
 
-
-	/*
-		public static void main(String[] args) {
-		double a = 5;
-		double b = 10;
-		int n = 7;
-		
-		Calculadora calc = new Calculadora();
-		
-		System.out.println(calc.suma(a, b));
-		System.out.println(calc.resta(a, b));
-		System.out.println(calc.mult(a, b));
-		System.out.println(calc.divide(a, b));
-		System.out.println(calc.fact(n));
-		System.out.println(calc.esPrimo(n));
-	}
-	
-	*/
 }
-
-
